4443c562c20ae801c8a196b093ce4d64cb612535
Jacqueline Chenault <adigitalnative@gmail.com>
2014-04-09 18:16:18 -0400

    Initial Rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActorExpenses::Application.config.secret_token = '9fd9c5990b07fda8c4b2f4723476ef27597887a42ab2535de64df5cf062f1ee5c162431095067165b7c8896a84d4bfa1429656f91b35801639a9a6396b324f37'
